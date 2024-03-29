56f346c870b8fa3a7e0a5d9b250851304318ac76
Faith-Nyambura <faithnyamburawamui@gmail.com>
2024-03-29 23:02:28 +0300

    made changes

---
+++ printemojis.py
@@ -0,0 +1,4 @@
+import emoji
+
+print(emoji.emojize("I love reading books:books:"))
+print(emoji.emojize("Some people are very funny heart:,purple_heart:, please be ineractive with them.:hibiscus"))
